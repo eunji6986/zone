991d1e3e1b5b22fb369e0d03b02ec72b2aa64bb2
LEE EUNJI <eunji6986@naver.com>
2022-05-27 02:47:44 +0900

    settings

---
+++ src/test/java/io/owl/zone/ZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.owl.zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
